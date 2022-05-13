48527f4cd99bcf0a95ee6b627d5e116eb67707dd
peterwoodworth <woodwoop@amazon.com>
2022-05-13 15:19:54 -0700

    init repository

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
-## My Project
+# team-assignment-manager
+Automatically assigns a member from a Github team to issues / PRs
 
-TODO: Fill this README out!
+Whoever from the given Github team has the fewest number of currently assigned issues / PRs from the team will be assigned to the issue when this action is ran
 
-Be sure to:
+# Inputs
 
-* Change the title in this README
-* Edit your repository description on GitHub
+## team
+  The team to assign to issues / PRs
+
+  *Required*
+
+## exempt-team
+  If the issue / PR was submitted by someone from this team, do not assign anyone to the issue
+
+  *default: does not check submitter*
+
+## github-token
+  The token must have read:org permission, so the default github token for the repo will not work.
+
+  *Required*
+
+# Example
+
+```yaml
+name: "Assigns members from team repo-dev to PRs"
+on:
+ pull_request_target:
+    types: [opened]
+
+jobs:
+  team-assignment-manager:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: aws-github-ops/aws-team-assignment-manager@v1.0.0
+        with:
+          github-token: "${{ secrets.MY_PAT }}"
+          team: "repo-dev"
+          exempt-team: "repo-team"
+```
+
 
 ## Security
 
---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+  const token = core.getInput('github-token');
+  const team = core.getInput('team');
+  const exemptTeam = core.getInput('exempt-team', { required: false });
+  const octokit = github.getOctokit(token);  
+  
+  // Check if issue was submitted by exempt team member
+  if (exemptTeam) {
+    const exemptMemberData = await octokit.rest.teams.listMembersInOrg({
+      org: github.context.repo.owner,
+      team_slug: exemptTeam,
+    });
+    for (const exemptMember of exemptMemberData.data) {
+      if (github.context.actor === exemptMember.login) {
+        core.info('Issue was submitted by exempt team member. Exiting successfully.');
+        return;
+      }
+    }
+  }
+
+  // set issue type to count
+  const thisIssueData = await octokit.rest.issues.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.issue.number,
+  });
+  let target = '';
+  if (thisIssueData.data.pull_request) {
+    target = 'pull_requests';
+  } else {
+    target = 'issues';
+  }
+
+  // get list of members on team
+  const memberData = await octokit.rest.teams.listMembersInOrg({
+    org: github.context.repo.owner,
+    team_slug: team,
+  });
+  const members = new Map<string, number>([]);
+  for (const member of memberData.data) {
+    members.set(member.login, 0);
+  }
+
+  // get number of issues/PRs assigned per team member
+  for (const [key, value] of members) {
+    const { data } = await octokit.rest.issues.listForRepo({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      assignee: key,
+      per_page: 100,
+    });
+    let count: number = 0;
+    for (const issue of data) {
+      if (validateIssue(issue, target)) ++count;
+    }
+    core.info(key + ' has ' + count.toString() + ' ' + target + ' assigned to them.');
+    members.set(key, count);
+  }
+
+  // determine team member with fewest assigned issues/PRs
+  let winner = '';
+  let low: number;
+  members.forEach((value: number, key: string) => {
+    if (winner === '') {
+      low = value;
+      winner = key;
+    } else {
+      if (value < low) {
+        low = value;
+        winner = key;
+      }
+    }
+  });
+
+  if (winner !== '') {
+    core.info('Assignee: ' + winner);
+    await octokit.rest.issues.addAssignees({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.issue.number,
+      assignees: [winner],
+    });
+  }
+}
+
+function validateIssue(issue, target: string): boolean {
+  // check issue state, issue must be 'open'
+  if (issue.state !== 'open') {
+    return false;
+  }
+
+  // check issue type
+  if (issue.pull_request && target.toLowerCase() === 'pull_requests') {
+    return true;
+  } else if (!issue.pull_request && target.toLowerCase() === 'issues') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+run().catch((error) => {
+  core.setFailed(error.message);
+});
